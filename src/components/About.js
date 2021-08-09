076ff9b738ef092cd9376df577d6c7c329c11581
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-09 17:28:04 -0400

    commit tree 43cce3bc01e264b4f583935bbce6608e10055c16

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {username,city,image} from "../data/user";
 
 function About() {
   return (
@@ -9,3 +10,5 @@ function About() {
     </div>
   );
 }
+
+export default About;
